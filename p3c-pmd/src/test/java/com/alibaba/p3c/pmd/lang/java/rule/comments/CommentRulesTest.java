ec6eb8722c884b025b76d678b298131838c04f11
可期 <keke.zkk@alibaba-inc.com>
2016-11-14 11:45:22 +0800

    Add AvoidApacheBeanUtilsCopy rule.

--- p3c-pmd/src/test/java/com/alibaba/p3c/pmd/lang/java/rule/comments/CommentRulesTest.java
+++ p3c-pmd/src/test/java/com/alibaba/p3c/pmd/lang/java/rule/comments/CommentRulesTest.java
@@ -2,10 +2,16 @@ package com.alibaba.p3c.pmd.lang.java.rule.comments;
 
 import net.sourceforge.pmd.testframework.SimpleAggregatorTst;
 
+/**
+ * ◊¢ ÕπÊ∑∂≤‚ ‘¿‡
+ * 
+ * @author keriezhang
+ *
+ */
 public class CommentRulesTest extends SimpleAggregatorTst {
 
     private static final String RULESET = "java-comments";
-    
+
     @Override
     public void setUp() {
         addRule(RULESET, "CommentsMustBeJavadocFormatRule");
---
+++ p3c-pmd/src/test/java/com/alibaba/p3c/pmd/lang/java/rule/others/OtherRulesTest.java
@@ -0,0 +1,19 @@
+package com.alibaba.p3c.pmd.lang.java.rule.others;
+
+import net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+
+/**
+ * 其他类规范测试类
+ * 
+ * @author keriezhang
+ *
+ */
+public class OtherRulesTest extends SimpleAggregatorTst {
+
+    private static final String RULESET = "java-others";
+
+    @Override
+    public void setUp() {
+        addRule(RULESET, "AvoidApacheBeanUtilsCopyRule");
+    }
+}
