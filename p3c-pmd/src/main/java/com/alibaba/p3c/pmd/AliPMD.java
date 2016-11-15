378c64ff27caef9b10349175f1fb1582d5efd1e7
昶乐 <changle.lq@alibaba-inc.com>
2016-11-15 15:31:30 +0800

    pmd test

--- p3c-pmd/src/main/java/com/alibaba/p3c/pmd/AliPMD.java
+++ p3c-pmd/src/main/java/com/alibaba/p3c/pmd/AliPMD.java
@@ -1,9 +1,9 @@
 package com.alibaba.p3c.pmd;
 
+import net.sourceforge.pmd.PMD;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import net.sourceforge.pmd.PMD;
 
 /**
  * 本地开发测试专用类，选择要检查的文件，以及对应的规则
@@ -18,17 +18,11 @@ public class AliPMD {
         List<String> list = new ArrayList<>();
 
         list.add("-d");
-        list.add("/Users/keriezhang/Documents/program/gitlab/"
-                + "p3c/alibaba-checkstyle-checks/src/test/java/com/alibaba/pmd/comments/"
-                + "MethodOfInterfaceMustUseJavaDocTest.java");
+        list.add("/Users/lq/project/gitrepos/p3c/p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/example");
 
         list.add("-R");
-        list.add("/Users/keriezhang/Documents/program/gitlab/"
-                + "p3c/alibaba-checkstyle-checks/src/main/resources/com/alibaba/pmd/"
-                + "abstract-method-and-interface-must-use-java-doc.xml");
+        list.add("/Users/lq/project/gitrepos/p3c/p3c-pmd/src/main/resources/rulesets/java/mycustomrules.xml");
 
-        // list.add("-f");
-        // list.add("xml");
 
         String[] myArgs = list.toArray(new String[list.size()]);
         PMD.main(myArgs);
--- p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/example/WhileLoopsMustUseBracesRule.java
+++ p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/example/WhileLoopsMustUseBracesRule.java
@@ -1,23 +1,11 @@
 package com.alibaba.p3c.pmd.lang.java.rule.example;
 
-import net.sourceforge.pmd.lang.ast.Node;
-import net.sourceforge.pmd.lang.java.ast.ASTBlock;
 import net.sourceforge.pmd.lang.java.ast.ASTWhileStatement;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 
-public class WhileLoopsMustUseBracesRule extends AbstractJavaRule{
-    
-    @Override
+public class WhileLoopsMustUseBracesRule extends AbstractJavaRule {
     public Object visit(ASTWhileStatement node, Object data) {
-        Node firstStmt = node.jjtGetChild(1);
-        if (!hasBlockAsFirstChild(firstStmt)) {
-            addViolation(data, node);
-        }
-        return super.visit(node, data);
+        System.out.println("hello world");
+        return data;
     }
-    
-    private boolean hasBlockAsFirstChild(Node node) {
-        return (node.jjtGetNumChildren() != 0 && (node.jjtGetChild(0) instanceof ASTBlock));
-    }
-
 }
