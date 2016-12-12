62f12256cc9f0afc5559734d960ef12c7443fabe
zenghou.fw <zenghou.fw@alibaba-inc.com>
2016-12-12 10:58:38 +0800

    fix:OOP规则误判及NPE
    Reviewers: 玄坛, 昶乐, 可期, 胜燕, 骏烈, P506186
    
    Reviewed By: 骏烈, P506186
    
    Differential Revision: https://aone.alibaba-inc.com/code/D75257

--- p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/oop/PojoMustUsePrimitiveFieldRule.java
+++ p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/oop/PojoMustUsePrimitiveFieldRule.java
@@ -4,6 +4,7 @@ import com.alibaba.p3c.pmd.lang.java.rule.AbstractPojoRule;
 import net.sourceforge.pmd.lang.ast.Node;
 import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTFieldDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTType;
 import org.jaxen.JaxenException;
 
 import java.util.List;
@@ -14,6 +15,8 @@ import java.util.List;
  * 1） 所有的POJO类属性使用包装数据类型。
  * 2） RPC方法的返回值和参数必须使用包装数据类型。
  * 3） 所有的局部变量使用基本数据类型。
+ *
+ * 只对1)进行检查
  *
  * @author zenghou.fw
  */
@@ -28,9 +31,12 @@ public class PojoMustUsePrimitiveFieldRule extends AbstractPojoRule {
 
                 for (Node fieldNode : fields) {
                     ASTFieldDeclaration field = (ASTFieldDeclaration)fieldNode;
-                    if (!field.isPublic() && !field.isStatic() && !field.isTransient() &&
-                            field.getType().isPrimitive()) {
-                        addViolation(data, field);
+                    if (!field.isPublic() && !field.isStatic() && !field.isTransient()) {
+                        Class type = field.getType();
+                        // TODO 在文本件内能找到名称的才进行检查,为null时是跨文件的就无能为力了
+                        if (type != null && type.isPrimitive()) {
+                            addViolation(data, field);
+                        }
                     }
                 }
             } catch (JaxenException e) {
