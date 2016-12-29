2ae7a962e857a962c4ee83a57afe6354224f263f
昶乐 <changle.lq@alibaba-inc.com>
2016-12-29 13:11:25 +0800

    fix ex
    Reviewers: P506186, 玄坛
    
    Reviewed By: P506186, 玄坛
    
    Subscribers: 玄坛
    
    Differential Revision: https://aone.alibaba-inc.com/code/D86752

--- p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/naming/ExceptionClassShouldEndWithExceptionRule.java
+++ p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/naming/ExceptionClassShouldEndWithExceptionRule.java
@@ -1,16 +1,33 @@
 package com.alibaba.p3c.pmd.lang.java.rule.naming;
 
-import net.sourceforge.pmd.lang.rule.XPathRule;
+import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceType;
+import net.sourceforge.pmd.lang.java.ast.ASTExtendsList;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 
 /**
- * @author changle.lq@alibaba-inc.com 2016/11/23 下午5:39
- * 异常类命名使用Exception结尾；
+ * @author changle.lq@alibaba-inc.com 2016/11/23 下午5:39 异常类命名使用Exception结尾；
  */
-public class ExceptionClassShouldEndWithExceptionRule extends XPathRule {
-    private static final String XPATH = "//ClassOrInterfaceDeclaration\n"
-        + "[//ClassOrInterfaceType[pmd-java:typeof(@Image, 'Exception')]]\n" + "[not (ends-with(@Image, 'Exception'))]";
-
-    public ExceptionClassShouldEndWithExceptionRule() {
-        super(XPATH);
+public class ExceptionClassShouldEndWithExceptionRule extends AbstractJavaRule {
+    @Override
+    public Object visit(ASTClassOrInterfaceDeclaration node, Object data) {
+        if (node.isInterface()) {
+            return super.visit(node, data);
+        }
+        ASTExtendsList astExtendsList =  node.getFirstChildOfType(ASTExtendsList.class);
+        if (astExtendsList != null) {
+            ASTClassOrInterfaceType  astClassOrInterfaceType = astExtendsList.getFirstChildOfType(ASTClassOrInterfaceType.class);
+            if (astClassOrInterfaceType != null) {
+                Class<?> astClassOrInterfaceTypeClass = astClassOrInterfaceType.getType();
+                if (astClassOrInterfaceTypeClass.isAssignableFrom(Exception.class) || astClassOrInterfaceTypeClass.isAssignableFrom(Throwable.class)) {
+                    if (!(node.getImage().endsWith("Exception"))) {
+                        addViolation(data, node);
+                    }
+                }
+            }
+        }
+        
+        return super.visit(node, data);
     }
+    
 }
