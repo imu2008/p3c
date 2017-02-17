5686259c7d6f94202e108d2824748144d0d96352
可期 <keke.zkk@alibaba-inc.com>
2017-02-17 11:52:39 +0800

    排除template、velocity以外的路径，忽略macro
    Summary: ref T9973820, T10054012
    
    Reviewers: P506186, 玄坛
    
    Reviewed By: P506186, 玄坛
    
    Differential Revision: https://aone.alibaba-inc.com/code/D113589

--- p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/vm/rule/others/UseQuietReferenceNotationRule.java
+++ p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/vm/rule/others/UseQuietReferenceNotationRule.java
@@ -1,22 +1,61 @@
 package com.alibaba.p3c.pmd.lang.vm.rule.others;
 
-import net.sourceforge.pmd.lang.rule.XPathRule;
+import net.sourceforge.pmd.RuleContext;
+import net.sourceforge.pmd.lang.vm.ast.ASTDirective;
+import net.sourceforge.pmd.lang.vm.ast.ASTReference;
+import net.sourceforge.pmd.lang.vm.ast.ASTSetDirective;
+import net.sourceforge.pmd.lang.vm.rule.AbstractVmRule;
+
+import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * 【强制】后台输送给页面的变量必须加$!{var}——中间的感叹号。
- * 
+ *
  * @author keriezhang
  * @date 2016年12月14日 上午11:24:59
- *
  */
-public class UseQuietReferenceNotationRule extends XPathRule {
+public class UseQuietReferenceNotationRule extends AbstractVmRule {
     /**
-     * set语句排除，其他语句均做判断。
+     * 执行扫描的文件路径
      */
-    private static final String XPATH =
-            "//Reference[matches(@literal, \"\\$[^!]+\") and not(ancestor::SetDirective)]";
+    private static final Pattern ALLOW_FILE_PATTERN = Pattern.compile(".*(template|velocity).*");
 
-    public UseQuietReferenceNotationRule() {
-        super(XPATH);
+    @Override
+    public Object visit(ASTReference node, Object data) {
+
+        // 排除template和velocity目录以外的文件
+        RuleContext ruleContext = (RuleContext) data;
+        String sourceCodeFilename = ruleContext.getSourceCodeFilename();
+
+        // 文件名既不为n/a（单元测试），又不包含template、velocity，则排除。
+        if (!"n/a".equals(sourceCodeFilename) && !ALLOW_FILE_PATTERN.matcher(sourceCodeFilename).matches()) {
+            return super.visit(node, data);
+        }
+
+        // 排除set语句
+        List<ASTSetDirective> setParents = node.getParentsOfType(ASTSetDirective.class);
+        boolean hasSetParent = !setParents.isEmpty();
+
+        // 排除macro宏定义
+        List<ASTDirective> directiveParents = node.getParentsOfType(ASTDirective.class);
+        boolean hasMacroParent = false;
+
+        for (ASTDirective directiveParent : directiveParents) {
+            if ("macro".equals(directiveParent.getDirectiveName())) {
+                hasMacroParent = true;
+                break;
+            }
+        }
+
+        // 既不在set语句下，也不在macro语句下，添加错误消息
+        if (!hasSetParent && !hasMacroParent) {
+            String literal = node.literal();
+            if (!literal.startsWith("$!")) {
+                addViolation(data, node);
+            }
+        }
+
+        return super.visit(node, data);
     }
 }
